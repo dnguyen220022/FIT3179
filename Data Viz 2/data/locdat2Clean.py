12057672c355dacdb27bd2ef44b0b2567b878b9e
Daniel Nguyen <dnguyen220022@gmail.com>
2024-10-07 13:02:33 +1100

    finish data cleaning

--- Data Viz 2/data/locdat2Clean.py
+++ Data Viz 2/data/locdat2Clean.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
-data = pd.read_csv("locdat2Original.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/dnguyen220022/FIT3179/refs/heads/main/Data%20Viz%202/data/locdat2Original.csv", encoding = "latin1")
 
 #drop rows with wrong data
 data = data.drop(data[~data["Incident.month"].isin(range(1,13))].index)
-data = data.drop(data[~data["Incident.year"].apply(lambda x: isinstance(x, int))].index)
-data = data.drop(data[~data["Latitude"].apply(lambda x: isinstance(x, float))].index)
-data = data.drop(data[~data["Longitude"].apply(lambda x: isinstance(x, float))].index)
+print("done")
+data["Incident.year"] = pd.to_numeric(data["Incident.year"], errors='coerce')
+data["Latitude"] = pd.to_numeric(data["Latitude"], errors='coerce')
+data["Longitude"] = pd.to_numeric(data["Longitude"], errors='coerce')
+data = data.dropna(subset=["Incident.year", "Latitude", "Longitude"])
+print("done")
 data = data.drop(data[data["Injury.location"] == "minor lacerations"].index)
+print("done")
 data = data.drop(data[data["Injury.severity"] == "male"].index)
-data = data.drop(data[data["Injury.severity"] == "male"].index)
+print("done")
 data = data.drop(data[~data["Victim.gender"].isin(["male", "female"])].index)
+print("done")
+data.to_csv("Data Viz 2\\data\\locdat2.csv", index=False)
